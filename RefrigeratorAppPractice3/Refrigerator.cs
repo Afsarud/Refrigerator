5dc0565fbb5599b2ae950344eab8912e5dff9fc1
Afsarud <engafsar5577@gmail.com>
2019-10-16 23:51:46 +0600

    Refrigerator

---
+++ RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    class Refrigerator
+    {
+
+        private double MaxWeight;
+        List<int> noItems = new List<int>();
+        List<double> unitWeights = new List<double>();
+
+        public Refrigerator(double maxWeight)
+        {
+            this.MaxWeight = maxWeight;
+        }
+        
+        public int NoItem { set; get; }
+        public double UnitWeight { set; get; }
+
+        public void Add(int noItem, double unitWeight)
+        {
+
+            noItems.Add(this.NoItem);
+            unitWeights.Add(this.UnitWeight);
+        }
+        
+        public double GetCurrentWeight()
+        {
+            double CurrentWeight = 0.0;
+            for (int i = 0; i < noItems.Count(); i++)
+            {
+                CurrentWeight += noItems[i] * unitWeights[i];
+
+            }
+            return CurrentWeight;
+        }
+        
+        public double GetRemainingWeight()
+        {
+            double RemainingWeight = this.MaxWeight - GetCurrentWeight();
+
+            return RemainingWeight;
+        }
+        
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        Refrigerator refrigerator;
+
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+        
+        private void SaveButton(object sender, EventArgs e)
+        {
+            refrigerator = new Refrigerator(Convert.ToDouble(maxWeightTakeTextBox.Text));
+            maxWeightTakeTextBox.Clear();
+
+        }
+
+        private void EnterButton(object sender, EventArgs e)
+        {
+           
+            refrigerator.NoItem = Convert.ToInt32(itemTextBox.Text);
+            refrigerator.UnitWeight = Convert.ToDouble(weightTextBox.Text);
+
+            
+            refrigerator.Add(refrigerator.NoItem, refrigerator.UnitWeight);
+
+            currentWeightTextBox.Text = Convert.ToString(refrigerator.GetCurrentWeight());
+
+            remainingWeightTextBox.Text = Convert.ToString(refrigerator.GetRemainingWeight());
+
+            itemTextBox.Clear();
+            weightTextBox.Clear();
+        }
+        
+    }
+}
